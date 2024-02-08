f13c532a3ebed53fe06306aeb767da27093fd4a8
sweettry <105031615+sweettry@users.noreply.github.com>
2024-02-07 22:09:27 -0600

    added Dashboard components
    created components: Links Dropdown, Navbar, Sidebar, Theme Toggle. Created dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,22 @@
-function layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+
+import { PropsWithChildren } from 'react';
+
+function layout({ children }: PropsWithChildren) {
+  return (
+    <main className='grid lg:grid-cols-5'>
+      {/* first-col hide on small screen */}
+      <div className='hidden lg:col-span-1 lg:block lg:min-h-screen'>
+        <Sidebar />
+      </div>
+      {/* second-col hide dropdown on big screen */}
+
+      <div className='lg:col-span-4'>
+        <Navbar />
+        <div className='px-4 py-16 sm:px-8 lg:px-16'>{children}</div>
+      </div>
+    </main>
+  );
 }
 export default layout;
