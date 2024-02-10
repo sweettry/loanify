3ae626d2fc322c9f43818d6446639655d1862089
sweettry <105031615+sweettry@users.noreply.github.com>
2024-02-09 20:02:51 -0600

    dev Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,18 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import LinksDropdown from './LinksDropdown';
+import { UserButton } from '@clerk/nextjs';
+import ThemeToggle from './ThemeToggle';
+
+function Navbar() {
+  return (
+    <nav className='bg-muted flex items-center justify-between px-4 py-4 sm:px-16 lg:px-24'>
+      <div>
+        <LinksDropdown />
+      </div>
+      <div className='flex items-center gap-x-4'>
+        <ThemeToggle />
+        <UserButton afterSignOutUrl='/' />
+      </div>
+    </nav>
+  );
+}
 export default Navbar;
