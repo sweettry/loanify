4e753baf9df9b6cb9c3d3eb33fed21764b4a42fb
sweettry <105031615+sweettry@users.noreply.github.com>
2024-02-09 19:58:27 -0600

    dev Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,32 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
-};
+'use client';
+import Logo from '@/assets/logo.svg';
+import links from '@/utils/links';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
+function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <aside className='bg-muted h-full px-8 py-4'>
+      <Image src={Logo} alt='logo' className='mx-auto' />
+      <div className='mt-20 flex flex-col gap-y-4'>
+        {links.map((link) => {
+          return (
+            <Button
+              asChild
+              key={link.href}
+              variant={pathname === link.href ? 'default' : 'link'}
+            >
+              <Link href={link.href} className='flex items-center gap-x-2 '>
+                {link.icon} <span className='capitalize'>{link.label}</span>
+              </Link>
+            </Button>
+          );
+        })}
+      </div>
+    </aside>
+  );
+}
 export default Sidebar;
