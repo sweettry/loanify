86037c777962d9bb3f12f6b2154a7b9ccf3506bb
sweettry <105031615+sweettry@users.noreply.github.com>
2024-02-09 22:30:33 -0600

    dev CustomFormComponents

---
+++ components/FormComponents.tsx
@@ -0,0 +1,83 @@
+import { Control } from 'react-hook-form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from './ui/input';
+
+type CustomeFormFieldProps = {
+  name: string;
+  control: Control<any>;
+};
+
+export function CustomFormField({ name, control }: CustomeFormFieldProps) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='capitalize'>{name}</FormLabel>
+          <FormControl>
+            <Input {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+
+type CustomeFormSelectProps = {
+  name: string;
+  control: Control;
+  items: string[];
+  labelText?: string;
+};
+export function CustomFormSelect({
+  name,
+  control,
+  items,
+  labelText,
+}: CustomeFormSelectProps) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => {
+        return (
+          <FormItem>
+            <FormLabel className='capitalize'>{labelText || name}</FormLabel>
+            <Select onValueChange={field.onChange} defaultValue={field.value}>
+              <FormControl>
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>
+                {items.map((item) => {
+                  return (
+                    <SelectItem key={item} value={item}>
+                      {item}
+                    </SelectItem>
+                  );
+                })}
+              </SelectContent>
+            </Select>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
+  );
+}
